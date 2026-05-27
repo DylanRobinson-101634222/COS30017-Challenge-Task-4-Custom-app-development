1f4711d35c854a903bd349e9c108f00966bafdf5
TheSpicyTacoGuy <drskyrim1@gmail.com>
2026-05-27 19:14:46 +1000

    fix: update app label to use string resource and clean up unused strings

--- app/src/main/java/com/aquatrack/app/util/ReminderFrequencyUtils.kt
+++ app/src/main/java/com/aquatrack/app/util/ReminderFrequencyUtils.kt
@@ -1,6 +1,5 @@
 package com.aquatrack.app.util
 
-@Suppress("unused")
 object ReminderFrequencyUtils {
     private const val CUSTOM_PREFIX = "Custom:"
     private const val CUSTOM_VALUE_SEPARATOR = ":"
